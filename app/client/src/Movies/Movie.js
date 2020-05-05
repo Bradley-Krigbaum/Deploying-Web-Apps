fa2faf8bc1549a7dbe3d740a8b7efabe7c63cb30
Bradley-Krigbaum <bradley.krigbaum@gmail.com>
2020-05-04 17:55:50 -0700

    added master by accident

--- app/client/src/Movies/Movie.js
+++ app/client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, Link, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+
+  const history = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -16,6 +18,16 @@ function Movie({ addToSavedList }) {
 
   const saveMovie = () => {
     addToSavedList(movie);
+  };
+
+  const deleteMovie = () => {
+    axios
+    .delete(`http://localhost:5000/api/movies/${movie.id}`)
+    .then(response => console.log("bk: Movie.js: deleteMovie: Deleted Movie Response: ", response))
+    .catch(err => console.log("bk: Movie.js: deleteMovie: Deleted Movie Error: ", err));
+
+    history.push('/');
+    window.location.reload();
   };
 
   useEffect(() => {
@@ -33,6 +45,18 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+
+      <div>
+        <Link className="add-movie-button" to={`/update-movie/${movie.id}`}>
+          Update
+        </Link>
+          
+        <div className="add-movie-button" onClick={deleteMovie}>
+          Delete
+
+        </div>
+      </div>
+
     </div>
   );
 }
